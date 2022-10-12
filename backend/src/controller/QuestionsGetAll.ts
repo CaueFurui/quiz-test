8cc407b2535ccb393164ba4e27901bba398d3ced
Gabriel Braga <44377180+gabrielmkt@users.noreply.github.com>
2022-10-11 22:21:59 -0300

    Delete backend

--- backend/src/controller/QuestionsGetAll.ts
+++
@@ -1,8 +0,0 @@
-import Questions from '../database/questions.json';
-import {Request, Response} from "express";
-
-export async function getAllQuestions(_: Request, response: Response) {
-  return response.send({ 
-    questions: Questions,
-  });
-}
